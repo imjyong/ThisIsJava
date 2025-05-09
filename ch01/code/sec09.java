675421eec5922133d6d6da965715c8b4c3215a39
J <162413448+imjyong@users.noreply.github.com>
2025-05-10 01:41:57 +0900

    Create sec09.java

---
+++ ch01/code/sec09.java
@@ -0,0 +1,11 @@
+// project: ThisIsJava
+// package: ch01.sec09
+// class: Hello
+
+package ch01.sec09;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+}
