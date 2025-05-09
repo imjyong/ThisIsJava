8109d2307e14fcef5122bdf8cffd66b696f787b5
J <162413448+imjyong@users.noreply.github.com>
2025-05-10 03:13:30 +0900

    Create ByteExample.java

---
+++ ch02/code/sec02/ByteExample.java
@@ -0,0 +1,10 @@
+package ch02.sec02;
+
+public class ByteExample {
+	public static void main(String[] args) {
+		byte var1 = -128;
+		// byte var2 = 128; // cannot convert from int to byte, 허용 범위 초과
+		
+		System.out.println(var1);
+	}
+}
