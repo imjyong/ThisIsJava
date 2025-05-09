335212739b6f68d5ee03c759eff2d2cfc7d22387
J <162413448+imjyong@users.noreply.github.com>
2025-05-10 02:51:50 +0900

    Create sec01.java

---
+++ ch02/code/sec01.java
@@ -0,0 +1,15 @@
+package ch02.sec01;
+
+/*
+project: ThisIsJava
+package: ch02.sec01
+*/
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		int value;
+		int result = value + 10; // 오류
+		
+		System.out.println(result);
+	}
+}
