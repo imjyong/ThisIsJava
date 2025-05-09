8ab71614e4576736c832eb9291c0915400539165
J <162413448+imjyong@users.noreply.github.com>
2025-05-10 02:20:08 +0900

    Update sec09.java

--- ch01/code/sec09.java
+++ ch01/code/sec09.java
@@ -1,10 +1,13 @@
-// project: ThisIsJava
-// package: ch01.sec09
-// class: Hello
-
 package ch01.sec09;
 
+/*
+project: ThisIsJava
+package: ch01.sec09
+*/
+
+// class
 public class Hello {
+	// method
 	public static void main(String[] args) {
 		System.out.println("Hello, Java");
 	}
